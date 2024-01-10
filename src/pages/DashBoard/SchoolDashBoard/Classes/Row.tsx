388f9ba40b3e803725a75e73d798f35cde191d9b
jimatth121 <jimatth121@gmail.com>
2024-01-10 14:28:53 +0100

    wip

--- src/pages/DashBoard/SchoolDashBoard/Classes/Row.tsx
+++ src/pages/DashBoard/SchoolDashBoard/Classes/Row.tsx
@@ -10,36 +10,35 @@ import { useState } from "react";
 import ChangeProfileStatus from "../Teachers/ChangeProfileStatus";
 import { TClassList } from "./Classes";
 
-
-
-
-
 const Row = ({
   data,
   onClick,
-  status
+  status,
 }: {
   data: TClassList;
   onClick?: () => void;
-  status?:string
+  status?: string;
 }) => {
-const [currentClicked, setCucrrentClicked] = useState(0);
-  
+  const [currentClicked, setCucrrentClicked] = useState(0);
+
   const queryClient = useQueryClient();
 
-  const {mutate, isLoading} = useDisableClass()
-  const {mutate:mutateActiveClass, isLoading:activeLoading} = useActiveClass()
+  const { mutate, isLoading } = useDisableClass();
+  const { mutate: mutateActiveClass, isLoading: activeLoading } =
+    useActiveClass();
 
-  const handleDisableClass = async ()=>{
-    if(status === ""){
-mutate({class_id:currentClicked},  {
+  const handleDisableClass = async () => {
+    if (status === "active") {
+      mutate(
+        { class_id: currentClicked },
+        {
           onSuccess(data) {
-             queryClient.invalidateQueries({ queryKey: ['GetClassList']});
+            queryClient.invalidateQueries({ queryKey: ["GetClassList"] });
             notifications.show({
               title: `Notification`,
               message: data.data.message,
             });
-            close()
+            close();
           },
 
           onError(err) {
@@ -48,17 +47,19 @@ mutate({class_id:currentClicked},  {
               message: getApiErrorMessage(err),
             });
           },
-        })
-    }
-    else{
-      mutateActiveClass({class_id:currentClicked},  {
+        }
+      );
+    } else {
+      mutateActiveClass(
+        { class_id: currentClicked },
+        {
           onSuccess(data) {
-             queryClient.invalidateQueries({ queryKey: ['GetClassList']});
+            queryClient.invalidateQueries({ queryKey: ["GetClassList"] });
             notifications.show({
               title: `Notification`,
               message: data.data.message,
             });
-            close()
+            close();
           },
 
           onError(err) {
@@ -67,17 +68,16 @@ mutate({class_id:currentClicked},  {
               message: getApiErrorMessage(err),
             });
           },
-        })
+        }
+      );
     }
-  
-  }
+  };
 
   const [opened, { open, close }] = useDisclosure(false);
 
-
   return (
     <>
-     <Modal
+      <Modal
         radius={10}
         padding={30}
         size={"md"}
@@ -93,40 +93,55 @@ mutate({class_id:currentClicked},  {
         withCloseButton={false}
         centered
       >
-        <ChangeProfileStatus  onContinue={handleDisableClass} activeIsLoading={activeLoading} isLoading={isLoading}  onCancel={close} label="Class" />
-      </Modal>                                  
- <div className="   my-auto border-b-[2px] border-[#eee]  py-4 font-medium ">
-      <div >
-        <div className="grid  grid-cols-[100px_300px_1fr_1fr_150px]    pr-4 pl-8">
-          <div className="flex justify-start items-center ">
-            <span className=" ">
-              <img loading="lazy" src={Rectangle} alt="" />
-            </span>
-          </div>
-          <div onClick={onClick} className="flex hover:cursor-pointer items-center justify-start gap-2 ">
-            <span>{data.name}</span>
-          </div>
-          <div className="flex justify-start items-center ">
-            {data.student_count}
-          </div>
-          <div className="flex justify-start items-center ">
-            {data.teacher_count}
-          </div>
-          <div className="flex justify-end  gap-4  items-center">
-            <span>{/* <img loading="lazy" src={ToggleIcon} alt="" /> */}</span>
-            <button  onClick={()=>{
-                setCucrrentClicked(data.id)
-              open()
-            }} className="flex justify-center items-center gap-1 bg-[#8530C1] rounded p-1 px-2">
-             <span className="text-white">{status === "active" ? "Disable": "Enable"}</span>
-            </button>
-            <span></span>
+        <ChangeProfileStatus
+          onContinue={handleDisableClass}
+          activeIsLoading={activeLoading}
+          isLoading={isLoading}
+          onCancel={close}
+          label="Class"
+        />
+      </Modal>
+      <div className="   my-auto border-b-[2px] border-[#eee]  py-4 font-medium ">
+        <div>
+          <div className="grid  grid-cols-[100px_300px_1fr_1fr_150px]    pr-4 pl-8">
+            <div className="flex justify-start items-center ">
+              <span className=" ">
+                <img loading="lazy" src={Rectangle} alt="" />
+              </span>
+            </div>
+            <div
+              onClick={onClick}
+              className="flex hover:cursor-pointer items-center justify-start gap-2 "
+            >
+              <span>{data.name}</span>
+            </div>
+            <div className="flex justify-start items-center ">
+              {data.student_count}
+            </div>
+            <div className="flex justify-start items-center ">
+              {data.teacher_count}
+            </div>
+            <div className="flex justify-end  gap-4  items-center">
+              <span>
+                {/* <img loading="lazy" src={ToggleIcon} alt="" /> */}
+              </span>
+              <button
+                onClick={() => {
+                  setCucrrentClicked(data.id);
+                  open();
+                }}
+                className="flex justify-center items-center gap-1 bg-[#8530C1] rounded p-1 px-2"
+              >
+                <span className="text-white">
+                  {status === "active" ? "Disable" : "Enable"}
+                </span>
+              </button>
+              <span></span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
-   
   );
 };
 
