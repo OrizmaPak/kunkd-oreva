46834f8198c123f6472a656a44e81793dcf59c72
jimatth121 <jimatth121@gmail.com>
2024-01-19 15:28:59 +0100

    wip

--- src/pages/DashBoard/SchoolDashBoard/LogoutModal.tsx
+++ src/pages/DashBoard/SchoolDashBoard/LogoutModal.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 // import { getUserState } from "@/store/authStore";
 import useStore from "@/store/index";
 import { getProfileState } from "@/store/profileStore";
-
+import DeleteIcon from "@/assets/deleteicon24.png";
 const LogoutModal = ({ onCloseModal }: { onCloseModal: () => void }) => {
   const navigate = useNavigate();
   const [profiles, setProfiles] = useStore(getProfileState);
@@ -26,22 +26,28 @@ const LogoutModal = ({ onCloseModal }: { onCloseModal: () => void }) => {
       transition={{ duration: 1 }}
     >
       <div>
-        <h1 className="font-bold text-[25px] mb-8 text-center font-Recoleta leading-[30px] ">
-          Are you sure you want to Log out of your dashboard?
+        <div className="flex justify-center items-center mb-3">
+          <img src={DeleteIcon} alt="image" className="w-[60px] h-[60px]" />
+        </div>
+        <h1 className="font-bold text-[25px] mb-2 text-center  font-Inter leading-[30px] ">
+          Log out?
         </h1>
-
+        <p className=" font-InterReg my-4 text-center">
+          Are you sure you want to log out?
+        </p>
         <div className="flex justify-end gap-4 mb-5 ">
           <Button
+            varient="outlined"
             onClick={onCloseModal}
-            className="p-4 px-10 bg-[#FFF8F7] text-red-600 rounded-full flex-grow font-bold"
+            className="p-4 px-10  text-black  flex-grow font-bold"
           >
             Cancel
           </Button>
           <Button
             onClick={handLogOut}
-            className="p-4 px-10 bg-red-600 text-white rounded-full flex-grow"
+            className="p-4 px-10 bg-red-600 text-white  flex-grow"
           >
-            Logout
+            Yes
           </Button>
         </div>
       </div>
