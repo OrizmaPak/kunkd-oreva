0fb9c5db5c00c2f98ebdd365dcb3f59913860d56
Matthew Egbedokun <jimatth121@gmail.com>
2025-08-20 17:56:29 +0100

    hid from parent

--- src/pages/DashBoard/TeacherDashboard/Request/Request.tsx
+++ src/pages/DashBoard/TeacherDashboard/Request/Request.tsx
@@ -52,7 +52,7 @@ const Request = () => {
           <div>
             <h1 className="text-[24px] font-semibold">
               Request{" "}
-              <span className="text-[#8530C1] bg-[#FFF7FD] rounded-3xl py-1 px-4">
+              <span className="text-[#8530C1] bg-[#FFF7FD] rounded-3xl py-2 px-4">
                 {attemptConnectStudents?.length || 0}
               </span>
             </h1>
